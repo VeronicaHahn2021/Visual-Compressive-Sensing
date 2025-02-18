4f7b53298a23096d78c1f339be292bbdb11d2391
VeronicaHahn2021 <veronicahahn2021@gmail.com>
2025-02-18 15:37:45 -0800

    Average Mutual coherences edited

--- theta_experiments.py
+++ theta_experiments.py
@@ -29,56 +29,20 @@ plt.ion()
 
 #Load Images:
 # Represent image as numpy array to make it easier to process
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 10))
-
 small_img_arr = process_image(small_img, mode)
-ax1.imshow(small_img_arr)
-ax1.set_title("{img}".format(img = small_img.split('.')[0]))
-ax1.axis('off')
-#print(small_img_arr.shape)
 small_img_arr_gray = process_image(small_img, False) #change from 'gray' to False
-ax2.imshow(small_img_arr_gray, 'gray')
-#print(small_img_arr_gray.shape)
-ax2.set_title("{img} grayscaled".format(img = small_img.split('.')[0]))
-ax2.axis('off')
-#plt.show()
-
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 10))
 big_img_arr = process_image(big_img, mode)
-ax1.imshow(big_img_arr)
-ax1.set_title("{img}".format(img = big_img.split('.')[0]))
-ax1.axis('off')
-#print(big_img_arr.shape)
 big_img_arr_gray = process_image(big_img, False) #change from 'gray' to False
-ax2.imshow(big_img_arr_gray, 'gray')
-ax2.set_title("{img} grayscaled".format(img = big_img.split('.')[0]))
-ax2.axis('off')
-#print(big_img_arr_gray.shape)
-#plt.show()
+
 
 #Collect v1 observations from small image:
 V1_W_100, V1_y_100 = generate_V1_observation(small_img_arr_gray, num_cell_100, cell_size, sparse_freq)
 V1_W_300, V1_y_300 = generate_V1_observation(small_img_arr_gray, num_cell_300, cell_size, sparse_freq)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 10))
 
-fig.suptitle("V1 Grascaled Reconstruction")
-## Reconstruction with 100 number of cells grayscaled
-reconst_gray_100 = reconstruct(V1_W_100, V1_y_100, alpha)
-ax1.imshow(reconst_gray_100, 'gray')
-ax1.set_title("{num_cell} number of cells".format(num_cell = num_cell_100))
-ax1.axis("off")
-
-# ## Reconstruction with 300 number of cells grayscaled
-reconst_gray_300 = reconstruct(V1_W_300, V1_y_300, alpha)
-ax2.imshow(reconst_gray_300, 'gray')
-ax2.set_title("{num_cell} number of cells".format(num_cell = num_cell_300))
-ax2.axis("off")
-plt.subplots_adjust(top=1.35)
-#plt.show()
 
 #trying to print theta/figure out what i need to access it specifically
-#turn this inot a function so i can use it on gaussian vs v1 vs pixel?
+#turn this into a function so i can use it on gaussian vs v1 vs pixel?
 W = V1_W_100
 def generate_theta(W):
     '''
@@ -93,15 +57,11 @@ def generate_theta(W):
 
     num_cell, n, m = W.shape
     theta = fft.dctn(W.reshape(num_cell, n, m), norm = 'ortho', axes = [1, 2])
-    #print(theta.shape)
-    theta = theta.reshape(num_cell, n * m) # PASS INTO COHERENCE FUNCTION
-    print(theta.shape) 
+    theta = theta.reshape(num_cell, n * m) # PASS INTO COHERENCE FUNCTION 
+    return theta
 
-generate_theta(W)
-
-
-test = [[1,2,3], [4,5,6], [7,8,9]]
-print(test)
+#theta = generate_theta(W)
+#print(theta.shape)
 
 
 def compute_mutual_coherence(A) :
@@ -111,7 +71,8 @@ def compute_mutual_coherence(A) :
     Parameters
     ----------
 
-    A: 2d matrix with more than one columns
+    A: array_like
+        matrix with more than one column
 
     The how:
     1. normalize columns of A (divide each by its norm):
@@ -128,19 +89,93 @@ def compute_mutual_coherence(A) :
 
     '''
     col_norms = np.linalg.norm(A, axis=0) # col_norms is an array of norms per column (the 0th axis of A is the row?)
-    #print(col_norms)
     x = A / col_norms #x is an array the same shape as A, but all columns have been normalized
-    #print(x)
     M = x.T @ x #M is the matrix multiplication between x and itself, diagonals are 1 because columns have been normalized (x*x = 1)
                  #M is a matrix of dot products between each column
-    #print(M)
     np.fill_diagonal(M, 0) #replace diagonals with 0 so they don't screw up the max
-    #print(M)
 
-    print(np.abs(M).flatten().max())
+    #print(np.abs(M).flatten().max())
     return np.abs(M).flatten().max() #max of M
 
-#compute_mutual_coherence(theta)
+#compute_mutual_coherence(test)
 #plt.close() if stuff wont close, theta[:, x] for column x, imshow to bring up heatmaps of W
 #W[x,:,:] is the xth 30x30 measurement, colorbar to bring up the colorbar, x from 1-100
 #theta is W gone through the fourier transform, 
+
+#Idea: find average mutual coherence over multiple V1, gaussian, and pixel observations (since
+#they're randomized), compare
+
+def mutual_coherence_matrix(A, n, obs_type) :
+    '''
+    Create a list of n computed mutual coherence(MC) values for given observations A
+
+    A: array_like(?)
+        Image // ex) small_img
+
+    n: int 
+        how many MC should be collected from one image, 
+        with purpose of averaging and comparing
+
+    obs_type: str
+        'V1', 'pixel', 'gaussian', which observation type we're computing for 
+
+    The how:
+    1. Create array M, will be our final list of MCs
+    2. for n times, generate theta and compute mutual coherence depending on ovserbation type
+        add each MC value to M
+    3. return M - to be plotted
+    
+    '''
+
+    M = np.zeros(n)
+    i = 0
+    for i in range(n):
+        if obs_type == 'V1':
+            W, Y = generate_V1_observation(A, num_cell_100, cell_size, sparse_freq)
+            theta = generate_theta(W)
+            M[i] = compute_mutual_coherence(theta)
+        if obs_type == "pixel":
+            W, Y = generate_pixel_observation(A, num_cell_100)
+            theta = generate_theta(W)
+            M[i] = compute_mutual_coherence(theta)
+        if obs_type == "gaussian":
+            W, Y = generate_gaussian_observation(A, num_cell_100)
+            theta = generate_theta(W)
+            M[i] = compute_mutual_coherence(theta)
+    return M
+
+def average_mutual_coherence(A, n, obs_type) :
+    '''
+    Create an array of average mutual coherences given image A, number of averages to create n, and observation type
+
+    A: array_like(?)
+        Image // ex) small_img
+
+    n: int 
+        how many ave MC should be collected from one image, 
+        over that many runs
+
+    obs_type: str
+        'V1', 'pixel', 'gaussian', which observation type we're computing for
+    '''
+
+    ave = np.zeros(n)
+    for i in range(n):
+        M = mutual_coherence_matrix(A, n, obs_type)
+        ave[i] = np.average(M)
+    return ave
+
+
+num=3
+#plot num amount of average mutual coherences over num runs
+
+ave_v1 = average_mutual_coherence(small_img_arr_gray, num, 'V1')
+ave_pix = average_mutual_coherence(small_img_arr_gray, num, 'pixel')
+ave_gau = average_mutual_coherence(small_img_arr_gray, num, 'gaussian')
+plt.plot(ave_v1, label = 'V1', marker = 'o')
+plt.plot(ave_pix, label = "Pixel", marker = 'o')
+plt.plot(ave_gau, label = "Gaussian", marker = 'o')
+plt.title('Average Mutual Coherence')
+plt.ylabel('Ave MC')
+plt.legend()
+plt.show()
