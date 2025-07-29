43ed4f0d554775ab9a05a9d7bee3a83011cf5dec
VeronicaHahn2021 <veronicahahn2021@gmail.com>
2025-07-29 14:03:52 -0700

    modified plot and table coeffs

--- A_experiments/coeffs_table.py
+++ A_experiments/coeffs_table.py
@@ -10,66 +10,69 @@ from src.compress_sensing import *
 from src.utility import *
 from A_experiments.theta_exp_improved import *
 
-def high_freq_table(img_arr, obs_type, num_cell, cell_size = None, blob_size = None, center = None):
-    '''
-    Creates a table identifying which DCT basis frequencies are where
+'''
+Try to table the coordinates of the coefficient vectors with the
+dot products and frequencies. Do low coefficient frequencies create high
+dot products, leading to high MC?
 
-    Parameters
-    ----------
+If a frequency's coefficient is low but it creates a high dot product,
+then that frequency is not very useful for reconstruction but messes with MC.
+Can we discard these frequencies in our MC computation?
+'''
 
-    img_arr:
-        (n, m) shape image containing array of pixels
+def dot_matrix_simple(measurement_matrix):
+    design_matrix = generate_design_matrix(measurement_matrix)
+    col_norms = np.linalg.norm(design_matrix, axis=0)
+    x = design_matrix / col_norms 
+    M = x.T @ x
+    np.fill_diagonal(M, 0)
+    return np.abs(M)
 
-    obs_type: String
-        Observation technique that are going to be used to 
-        collect sample for reconstruction. Default set up to 'pixel'
-        Supported observation : ['pixel', 'gaussian', 'V1'].
 
-    num_cell : int
-        Number of blobs that will be used to be 
-        determining which pixels to grab and use.
 
-    cell_size : int
-        Determines field size of opened and closed blob of data. 
-        Affect the data training.
-        
-    blob_size : int
-        Determines filed frequency on how frequently 
-        opened and closed area would appear. 
-        Affect the data training.
-    '''
+coeffs_true = generate_coeff_vector(small_img_arr_gray, num_cell_300, cell_size, blob_size)
 
-    M = dot_vec = dot_product_matrix(img_arr, obs_type, num_cell, cell_size, blob_size, center)
-    M_vec = np.ravel(M) # dot product matrix as a vector
-    #M_coords will be the coordinates of each coherence in M, which
-    #we want to keep track of bc that'll tell us where the largest coherences are
-    n = M.shape[0]
-    M_coords = np.unravel_index(range(n**2), (n,n))
-    arr = np.arange(30)
-    kx = np.tile(arr, 30)
-    ky = np.repeat(arr, 30)
-    perm = np.argsort(M_vec)[::-1] # tells how to sort, reversed to decrease
-    M_vec[perm] # decreasing
-    i = M_coords[0][perm]
-    j = M_coords[1][perm]
-    # look into kx, ky to map i -> (kx, ky)
-    i_coords=[[]for s in range(i.shape[0])]
-    j_coords=[[]for s in range(j.shape[0])]
-    for m in range(i.shape[0]):
-        i_coords[m] = [kx[i[m]],ky[i[m]]] #assigns kx,ky coordinates to elements in i
-    for m in range(j.shape[0]):
-        j_coords[m] = [kx[j[m]],ky[j[m]]] #assigns kx,ky coordinates to elements in j
-    i_arr = [i,i_coords]
-    j_arr = [j,j_coords]
-    df = pd.DataFrame({
-        "i": i,
-        "j": j,
-        "(kx_i,ky_i)": i_coords,
-        "(kx_j,ky_j)": j_coords,
-        "MC": M_vec[perm]
-    })
-    return df
+measurement_matrix, V1_y_300 = generate_V1_observation(small_img_arr_gray, num_cell_300, cell_size, blob_size, None)
+reconst_gray_300 = reconstruct(measurement_matrix, V1_y_300, alpha)
+coeffs_est = generate_coeff_vector(reconst_gray_300, num_cell_300, cell_size, blob_size)
 
-    df = high_freq_table(small_img_arr_gray, "V1", num_cell_300, cell_size, blob_size)
-    display(df)
-    print("Hello world")
+M = dot_vec = dot_matrix_simple(measurement_matrix)
+M_vec = np.ravel(M) # dot product matrix as a vector
+#M_coords will be the coordinates of each coherence in M, which
+#we want to keep track of bc that'll tell us where the largest coherences are
+n = M.shape[0]
+M_coords = np.unravel_index(range(n**2), (n,n))
+arr = np.arange(30)
+kx = np.tile(arr, 30)
+ky = np.repeat(arr, 30)
+perm = np.argsort(M_vec)[::-1] # tells how to sort, reversed to decrease
+M_vec[perm] # decreasing
+i = M_coords[0][perm]
+j = M_coords[1][perm]
+# look into kx, ky to map i -> (kx, ky)
+i_coords=[[]for s in range(i.shape[0])]
+j_coords=[[]for s in range(j.shape[0])]
+for m in range(i.shape[0]):
+    i_coords[m] = [kx[i[m]],ky[i[m]]] #assigns kx,ky coordinates to elements in i
+for m in range(j.shape[0]):
+    j_coords[m] = [kx[j[m]],ky[j[m]]] #assigns kx,ky coordinates to elements in j
+i_arr = [i,i_coords]
+j_arr = [j,j_coords]
+
+'''
+How do we sort coeffs so that the entries are related to the (i,j) entries correctly?
+'''
+
+
+data = {
+    #"true coeffs": np.abs(coeffs_true).flatten(),
+    #"estimated coeffs": np.abs(coeffs_est).flatten(),
+    "i": i,
+    "j": j,
+    "(kx_i,ky_i)": i_coords,
+    "(kx_j,ky_j)": j_coords,
+    "MC": M_vec[perm]
+}
+
+df = pd.DataFrame(data)
+print(df)
--- A_experiments/plot_coeffs.py
+++ A_experiments/plot_coeffs.py
@@ -15,45 +15,64 @@ Plot the coefficient vectors for an image and its V1 reconstruction.
 '''
 
 coeffs_true = generate_coeff_vector(small_img_arr_gray, num_cell_300, cell_size, blob_size)
-bins=100
+bins=200
 
 V1_W_300, V1_y_300 = generate_V1_observation(small_img_arr_gray, num_cell_300, cell_size, blob_size, None)
-'''
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 10))
-# ## Reconstruction with 300 number of cells grayscaled
-ax1.imshow(small_img_arr_gray,'gray')
-ax1.set_title("Original")
-ax1.axis("off")
-
-reconst_gray_300 = reconstruct(V1_W_300, V1_y_300, alpha)
-ax2.imshow(reconst_gray_300, 'gray')
-ax2.set_title("Reconstruct{num_cell} number of cells".format(num_cell = num_cell_300))
-ax2.axis("off")
-plt.subplots_adjust(top=1.35)
-plt.show()
-'''
-
-reconst_gray_300 = reconstruct(V1_W_300, V1_y_300, alpha)
-coeffs_est = generate_coeff_vector(reconst_gray_300, num_cell_300, cell_size, blob_size)
-
-
-
+reconst_gray_300_v1 = reconstruct(V1_W_300, V1_y_300, alpha)
+coeffs_est_v1 = generate_coeff_vector(reconst_gray_300_v1, num_cell_300, cell_size, blob_size)
 plt.figure()
-plt.hist(np.abs(coeffs_true).flatten(), bins, cumulative = False, density = True, label = "True")
-plt.hist(np.abs(coeffs_est).flatten(), bins, cumulative = False, density = True, label = "Estimated")
-plt.xlabel("Coefficient Vectors")
-plt.ylabel("Frequency")
+plt.plot(coeffs_true.flatten(), "o", label = "True")#, bins, cumulative = False, density = True, label = "True")
+plt.plot(coeffs_est_v1.flatten(), "+", label = "Estimated")#, bins, cumulative = False, density = True, label = "Estimated")
+plt.xlabel("Index")
+plt.ylabel("Value")
+plt.ylim([1e-3, 1e4])
 plt.legend()
 plt.yscale('log')
+plt.title("V1")
 plt.show()
-plt.savefig("Coeffs Hist")
+#plt.savefig("Coeffs Hist")
 
+
+pixel_W_300, pixel_y_300 = generate_pixel_observation(small_img_arr_gray, num_cell_300)
+reconst_gray_300_pix = reconstruct(pixel_W_300, pixel_y_300, alpha)
+coeffs_est_pix = generate_coeff_vector(reconst_gray_300_pix, num_cell_300, cell_size, blob_size)
+plt.figure()
+plt.plot(coeffs_true.flatten(), "o", label = "True")#, bins, cumulative = False, density = True, label = "True")
+plt.plot(coeffs_est_pix.flatten(), "+", label = "Estimated")#, bins, cumulative = False, density = True, label = "Estimated")
+plt.xlabel("Index")
+plt.ylabel("Value")
+plt.ylim([1e-3, 1e4])
+plt.legend()
+plt.yscale('log')
+plt.title("Pixel")
+plt.show()
+
+
+gaussian_W_300, gaussian_y_300 = generate_gaussian_observation(small_img_arr_gray, num_cell_300)
+reconst_gray_300_gauss = reconstruct(gaussian_W_300, gaussian_y_300, alpha)
+coeffs_est_gauss = generate_coeff_vector(reconst_gray_300_gauss, num_cell_300, cell_size, blob_size)
+plt.figure()
+plt.plot(coeffs_true.flatten(), "o", label = "True")#, bins, cumulative = False, density = True, label = "True")
+plt.plot(coeffs_est_pix.flatten(), "+", label = "Estimated")#, bins, cumulative = False, density = True, label = "Estimated")
+plt.xlabel("Index")
+plt.ylabel("Value")
+plt.ylim([1e-3, 1e4])
+plt.legend()
+plt.yscale('log')
+plt.title("Gaussian")
+plt.show()
+
+
+'''
 plt.figure()
 plt.title("True")
 plt.imshow(coeffs_true)
 plt.colorbar()
+plt.savefig("Coeffs True Heat")
 
 plt.figure()
 plt.title("Estimated")
 plt.imshow(coeffs_est)
 plt.colorbar()
+plt.savefig("Coeffs Est Heat")
+'''
