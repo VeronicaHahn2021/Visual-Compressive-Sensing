f1b9b0cbffb86c18a268696be5fe796d09c71abb
ruchig145 <guptar@wwu.edu>
2025-12-03 11:58:20 -0800

    Harris' changes to MSE and PC calc

--- A_experiments/SVD_coeffs.py
+++ A_experiments/SVD_coeffs.py
@@ -16,30 +16,28 @@ Compare the Fourier coefficients to singular values of theta.
 '''
 # Find the true coefficients of theta
 coeffs_true = generate_coeff_vector(small_img_arr_gray, num_cell_300, cell_size, blob_size)
-U_c_true, S_c_true, V_c_true = np.linalg.svd(coeffs_true)
-a_true = np.reshape(V_c_true.T * coeffs_true,-1)
+# U_c_true, S_c_true, Vh_c_true = np.linalg.svd(coeffs_true)
 
 
 # Find the singular values of theta
 measurement_matrix_V1, V1_y_300 = generate_V1_observation(small_img_arr_gray, num_cell_300, cell_size, blob_size, None)
 theta_V1 = generate_design_matrix(measurement_matrix_V1)
 
-U_V1, S_V1, V_V1 = np.linalg.svd(theta_V1)
+U_V1, S_V1, Vh_V1 = np.linalg.svd(theta_V1)
 
 measurement_matrix_pix, pixel_y_300 = generate_pixel_observation(small_img_arr_gray, num_cell_300)
 theta_pix = generate_design_matrix(measurement_matrix_pix)
 
-U_pix, S_pix, V_pix = np.linalg.svd(theta_pix)
+U_pix, S_pix, Vh_pix = np.linalg.svd(theta_pix)
 
 measurement_matrix_gauss, gaussian_y_300 = generate_gaussian_observation(small_img_arr_gray, num_cell_300)
 theta_gauss = generate_design_matrix(measurement_matrix_gauss)
 
-U_gauss, S_gauss, V_gauss = np.linalg.svd(theta_gauss)
+U_gauss, S_gauss, Vh_gauss = np.linalg.svd(theta_gauss)
 
 # Find the estimated coefficients for each observation type
 reconst_gray_300_v1 = reconstruct(measurement_matrix_V1, V1_y_300, alpha)
 coeffs_est_V1 = generate_coeff_vector(reconst_gray_300_v1, num_cell_300, cell_size, blob_size)
-
 
 reconst_gray_300_pix = reconstruct(measurement_matrix_pix, pixel_y_300, alpha)
 coeffs_est_pix = generate_coeff_vector(reconst_gray_300_pix, num_cell_300, cell_size, blob_size)
@@ -50,14 +48,16 @@ coeffs_est_gauss = generate_coeff_vector(reconst_gray_300_gauss, num_cell_300, c
 
 
 # find principal components from coefficient vectors for each obs type
-U_c_V1, S_c_V1, V_c_V1 = np.linalg.svd(coeffs_est_V1)
-a_est_V1 = np.reshape(V_c_V1 * coeffs_est_V1,-1)
+# get sensing matrix out out these and run again (must be 300 x 900)
+# U_c_V1, S_c_V1, Vh_c_V1 = np.linalg.svd(coeffs_est_V1) # run this w/ measurement_matrix_V1
+a_est_V1 = Vh_V1 @ coeffs_est_V1.flatten()
 
-U_c_pix, S_c_pix, V_c_pix = np.linalg.svd(coeffs_est_pix)
-a_est_pix = np.reshape(V_c_pix * coeffs_est_pix,-1)
 
-U_c_gauss, S_c_gauss, V_c_gauss = np.linalg.svd(coeffs_est_gauss)
-a_est_gauss = np.reshape(V_c_gauss * coeffs_est_gauss,-1)
+# U_c_pix, S_c_pix, Vh_c_pix = np.linalg.svd(coeffs_est_pix)
+a_est_pix = Vh_pix @ coeffs_est_pix.flatten()
+
+# U_c_gauss, S_c_gauss, Vh_c_gauss = np.linalg.svd(coeffs_est_gauss)
+a_est_gauss = Vh_gauss @ coeffs_est_gauss.flatten()
 
 # Plot principal components
 
@@ -86,18 +86,22 @@ def make_scatter(est, true, xlabel, title, filename, figsize=(10, 8), dpi=200, m
     plt.savefig(filename)
     plt.close()
 
+a_true_V1 = Vh_V1 @ coeffs_true.flatten()
+
 #make_scatter(a_est_V1,   a_true, "V1", "V1 vs True (100 samples)", "V1_vs_True_100_YlOrRd.png", alpha=0.5)
 #make_scatter(a_est_pix,  a_true, "Pixel", "Pixel vs True (100 samples)", "Pixel_vs_True_100_YlOrRd.png")
 #make_scatter(a_est_gauss, a_true, "Gaussian", "Gaussian vs True (100 samples)", "Gaussian_vs_True_100_YlOrRd.png")
 
 
-# mean squared error
-squared_error = np.empty(900)
+# # mean squared error
+# squared_error = np.empty(900)
 
-for i in range(900):
-    squared_error[i] = (a_true[i] - a_est_V1[i]) ** 2
+# for i in range(900):
+#     squared_error[i] = (a_true[i] - a_est_V1[i]) ** 2
 
-mse = np.sum(squared_error) / 900
+squared_error = (a_true_V1 - a_est_V1) ** 2
+
+mse = np.mean(squared_error)
 
 im = process_image(small_img,False)
 true_pixels = np.array(im)
